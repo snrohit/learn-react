0d98b6880e3ea03a2f8d1feefefcb5c17cf35a67
Nair, Rohit S <rohit.s.nair@accenture.com>
2020-07-16 11:03:24 +0530

    React Animations

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -14,6 +14,7 @@ import { connect} from 'react-redux';
 
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 
 const mapDispatchToProps = dispatch => ({
@@ -74,14 +75,18 @@ class  Main extends Component {
     return (
       <div >
       <Header></Header>
-       <Switch>
+      <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch location={this.props.location}>
          <Route path='/home' component={Homepage}  />
          <Route exact path ='/menu' component={()=> <Menu dishes={this.props.dishes}></Menu>}></Route>
          <Route exact path='/contactus' component={Contact}/>
          <Route path='/menu/:dishId' component={DishWithId} />
          <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
          <Redirect to='/home'></Redirect>
-       </Switch>
+         </Switch>
+            </CSSTransition>
+          </TransitionGroup>
   
       <Footer></Footer>
     </div>
